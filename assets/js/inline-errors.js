f39108015b2c927e815b8447827564572a89af9f
Marc Jauvin <marc.jauvin@gmail.com>
2022-03-06 14:38:30 -0500

    convert to vanilla js

--- assets/js/inline-errors.js
+++ assets/js/inline-errors.js
@@ -1,7 +1,11 @@
-$(window).on('ajaxInvalidField', function(event, fieldElement, fieldName, errorMsg, isFirst) {
-    $(fieldElement).closest('.form-group').addClass('has-error');
-});
+window.addEventListener('ajaxInvalidField', function(event, fieldElement, fieldName, errorMsg, isFirst) {
+    fieldElement.closest('.form-group').classList.add('has-error')
+})
 
-$(document).on('ajaxPromise', '[data-request]', function() {
-    $(this).closest('form').find('.form-group.has-error').removeClass('has-error');
-});
+document.querySelectorAll('[data-request]').forEach(function(el) {
+    el.addEventListener('ajaxPromise', function(event, promise) {
+        el.closest('form').querySelectorAll('.form-group.has-error').forEach(function(errorElement) {
+            errorElement.classList.remove('has-error')
+        })
+    })
+})
